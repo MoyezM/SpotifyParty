f31367cdd00be7225b88134f03032d398bbbd484
Moyez <MoyezM@users.noreply.github.com>
2019-04-09 19:06:52 -0400

    Initial Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { PlayerComponent } from './player/player.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import {
+  MatButtonModule,
+  MatIconModule,
+  MatListModule,
+  MatToolbarModule,
+  MatInputModule,
+  MatProgressBarModule,
+} from '@angular/material';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainComponent,
+    PlayerComponent,
+    ToolbarComponent
+
+  ],
+  imports: [
+    MatToolbarModule,
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
